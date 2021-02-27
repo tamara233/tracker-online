b1014d2b7f0ee7b41bd21a4ed8fe091d7d66e413
Tamara Sirakanyan <tamarasirakanyan@tamsmac.local>
2021-02-27 20:11:39 +0400

    added files

---
+++ assets/js/main.js
@@ -0,0 +1,65 @@
+///////////////////////////////////////
+// Scrolling events
+// get scroll coords
+const section1 = document.getElementById("image-over1");
+const section2 = document.getElementById("users");
+
+const sectionToZoom = document.getElementById("banner");
+const sectionCoords = sectionToZoom.getBoundingClientRect();
+const sectionTop = Math.ceil(sectionCoords.top + window.pageYOffset);
+
+// const section2Coords = section2.getBoundingClientRect();
+// const section2Top = Math.ceil(section2Coords.top + window.pageYOffset);
+
+// On scroll add overlay
+document.addEventListener("scroll", () => {
+  if (
+    window.scrollY > sectionTop &&
+    window.scrollY < (sectionTop + sectionCoords.height) / 2
+  ) {
+    section1.classList.remove("hidden");
+  }
+});
+
+///////////////////////////////////////
+// Sticky navigation
+const nav = document.getElementById("sticky--nav");
+const navContainer = document.querySelector("#navigation");
+const navHeight = nav.getBoundingClientRect().height;
+
+window.onscroll = function () {
+  myFunction();
+};
+
+let sticky = nav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    nav.classList.add("stick");
+  } else {
+    nav.classList.remove("stick");
+  }
+}
+
+///////////////////////////////////////
+// Reveal sections
+// const allSections = document.querySelectorAll(".section");
+
+// const revealSection = function (entries, observer) {
+//   const [entry] = entries;
+
+//   if (!entry.isIntersecting) return;
+
+//   entry.target.classList.remove("section--hidden");
+//   observer.unobserve(entry.target);
+// };
+
+// const sectionObserver = new IntersectionObserver(revealSection, {
+//   root: null,
+//   threshold: 0.15,
+// });
+
+// allSections.forEach(function (section) {
+//   sectionObserver.observe(section);
+//   section.classList.add("section--hidden");
+// });
